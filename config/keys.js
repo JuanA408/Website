ff4af92ffad2579345556df36491e09c0a7c2fc3
Juan408AR <aguirre.juanmp15@gmail.com>
2023-06-12 22:54:55 -0700

    Protecting user routes with jwt and passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://juan408ar:xisjek-seksyR-9wyzxy@cluster0.ikycq3k.mongodb.net/?retryWrites=true&w=majority'
-}
+    mongoURI: 'mongodb+srv://juan408ar:xisjek-seksyR-9wyzxy@cluster0.ikycq3k.mongodb.net/?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
