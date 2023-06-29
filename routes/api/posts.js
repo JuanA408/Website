24de6e07c3baa20ff591ffbb29ad33e86c999746
Juan408AR <aguirre.juanmp15@gmail.com>
2023-06-29 12:26:28 -0700

    Adding liking post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -65,27 +65,6 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 // @desc    Delete post
 // @access  Private
 
-// router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     Profile.findOne({ user: req.user.id })
-//         .then(profile => {
-//             Post.findById(req.params.id)
-//                 .then(post => {
-//                     // Check for post owner
-//                     if(post.user.id.toString() !== req.user.id) {  // postID from params !== userID from db
-//                         return res.status(401).json({ notauthorized: 'User not authorized' });
-//                     }
-
-//                     // Delete
-//                     post.remove().then(() => {
-//                         res.json({ success: true })
-//                     });
-//                     // .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
-//                 })
-//                 .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
-//         });
-// });
-
-
 router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
     Profile.findOne({ user: req.user.id })
         .then(profile => {
@@ -96,8 +75,29 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
                     }
                     // Delete
                     // console.log(post.id);
-                    // post.remove().then(() => res.json({success: true})); 
                     Post.findOneAndRemove({ _id: post.id }).then(() => res.json({success: true})); 
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+        })
+});
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => { 
+                    if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                        return res.status(400).json({ alreadyliked: 'User already liked this post' });
+                    }
+
+                    // Add user id to likes array
+                    post.likes.unshift({ user: req.user.id });
+
+                    post.save().then(post => res.json(post));
                 })
                 .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
         })
