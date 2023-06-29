e8475ffbe0e3aa1b57726bd9273c1c52d0571d7d
Juan408AR <aguirre.juanmp15@gmail.com>
2023-06-29 09:21:21 -0700

    Adding delete post route feature

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Post model
 const Post = require('../../models/Post');
 
+// Profile model
+const Profile = require('../../models/Profile');
+
 // Validation
 const validatePostInput = require('../../validation/post');
 // @route   GET api/posts/test
@@ -58,6 +61,46 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     newPost.save().then(post => res.json(post));
 });
 
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
 
+// router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+//     Profile.findOne({ user: req.user.id })
+//         .then(profile => {
+//             Post.findById(req.params.id)
+//                 .then(post => {
+//                     // Check for post owner
+//                     if(post.user.id.toString() !== req.user.id) {  // postID from params !== userID from db
+//                         return res.status(401).json({ notauthorized: 'User not authorized' });
+//                     }
+
+//                     // Delete
+//                     post.remove().then(() => {
+//                         res.json({ success: true })
+//                     });
+//                     // .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+//                 })
+//                 .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+//         });
+// });
+
+
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => { // error occurs
+                    if(post.user.toString() !== req.user.id) {
+                        return res.status(401).json({ notauthorized: 'User not authorized'});
+                    }
+                    // Delete
+                    // console.log(post.id);
+                    // post.remove().then(() => res.json({success: true})); 
+                    Post.findOneAndRemove({ _id: post.id }).then(() => res.json({success: true})); 
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+        })
+});
 
 module.exports = router;
